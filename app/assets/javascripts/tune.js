f43a201058f6f33c1acf211e46400bb35a8838ab
ktblk <black.katherine.g@gmail.com>
2016-09-28 17:39:17 -0400

    added ajax for getting jsonp to tune.js to populate fontend

--- app/assets/javascripts/tune.js
+++ app/assets/javascripts/tune.js
@@ -1,2 +1,36 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function(){
+  $.ajax({
+    url: "https://stg-resque.hakuapp.com/albums.json",
+    method: "GET",
+    dataType: 'jsonp'
+
+  })
+  .done(function( data ) {
+    $.each(data, function(index, value){
+      console.log(index, value);
+
+
+      // console.info(value);
+    })
+  });
+
+})
+
+$(function(){
+  $.ajax({
+      url: "https://stg-resque.hakuapp.com/songs?album_id=1",
+      method: "GET",
+      dataType: 'jsonp'
+
+  })
+  .done(function( data ) {
+    $.each(data, function(index, value){
+      console.log(index, value)
+
+      // console.info(value);
+    })
+  });
+
+})
