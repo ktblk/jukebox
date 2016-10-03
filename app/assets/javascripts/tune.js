49e02525903c81c1ef4c7e40de0dfd8b90b892ef
ktblk <black.katherine.g@gmail.com>
2016-10-02 23:45:23 -0400

    edited tunes.js file for song panel completion; ordered songs; moved from panel to ul/li list because of lack of adaptiblity

--- app/assets/javascripts/tune.js
+++ app/assets/javascripts/tune.js
@@ -1,16 +1,88 @@
-function initCarousel(){
-  $('#boutique').boutique({
-    container_width:	700,
-    front_img_width:	170,
-    front_img_height:	160,
-    behind_opacity:	1,
-    back_opacity:	1,
-    hovergrowth:	0.0,
-    speed:	800,
-    behind_size:	1,
-      back_size:	1,
-    // move_on_click: true;
+$(function(){
+
+  $.ajax({
+    url: "https://stg-resque.hakuapp.com/albums.json",
+    method: "GET",
+    dataType: 'jsonp'
+
+  })
+  .done(function( data ) {
+    $.each(data, function(index, value){
+      $('#boutique').append(htmlFill(value));
+    })
+  })
+  .success(function(){
+    initCarousel();
   });
+  getSong(1);
+  $(document).on("click", "#star a", function(event) {
+    event.preventDefault();
+    var star = $(this).find('.fa-star');
+    console.log(star);
+    if (star.hasClass("yellow-star")) {
+      star.removeClass("yellow-star");
+    }else {
+      star.addClass("yellow-star");
+    }
+  });
+
+})
+
+function pre_move_callback(anchor, instanceid, frame){
+  getSong(frame);
+}
+
+function getSong(albumId){
+
+    $.ajax({
+        url: "https://stg-resque.hakuapp.com/songs?album_id=" + albumId,
+        method: "GET",
+        dataType: 'jsonp'
+    })
+    .done(function( data ) {
+      $('#songlist').html("");
+      data.sort(function (a, b) {
+        return a.song_order - b.song_order;
+      });
+      $.each(data, function(index, value){
+        $('#songlist').append(tableFill(value));
+      })
+    })
+    .success( function(){
+      $('[data-toggle="tooltip"]').tooltip({
+          container: 'body',
+          trigger: 'hover'
+      });
+    });
+
+}
+
+function tableFill(value){
+  var category = "";
+  if (value.song_label == null) {
+    category = "";
+  }else{
+    $.each(value.song_label, function(i,value){
+      category += '<span id="explicit">'+ value +'</span>';
+    })
+  }
+  return `<li class="list-group-item clearfix">
+      <span class="badge" id="badge-custom">${value.song_duration}</span>
+      <div class="col-sm-2 set-padding-0">
+          <div class="col-sm-6 text-center">
+            <span id="num">${value.song_order}</span>
+          </div>
+          <div class="col-sm-6 text-center set-padding-0" id="star">
+            <a href="#" data-placement="top" title="MARK AS FAVORITE" data-toggle="tooltip">
+              <i class="fa fa-star" aria-hidden="true" style="color: #DDDDDD; font-size: 18px"></i>
+            </a>
+          </div>
+      </div>
+      <div class="col-sm-9">
+        <span id="songname">${value.song_name}</span>
+        ${category}
+      </div>
+  </li>`;
 }
 
 function htmlFill(value){
@@ -30,90 +102,17 @@ function htmlFill(value){
 
 }
 
-$(function(){
-
-  $.ajax({
-    url: "https://stg-resque.hakuapp.com/albums.json",
-    method: "GET",
-    dataType: 'jsonp'
-
-  })
-  .done(function( data ) {
-    $.each(data, function(index, value){
-      console.info(value.id);
-
-      $('#boutique').append(htmlFill(value));
-    })
-  })
-  .success(function(){
-    initCarousel();
+function initCarousel(){
+  $('#boutique').boutique({
+    container_width:	600,
+    front_img_width:	170,
+    front_img_height:	160,
+    behind_opacity:	1,
+    back_opacity:	1,
+    hovergrowth:	0.0,
+    speed:	800,
+    behind_size:	1,
+    back_size:	1,
+    move_on_click: true
   });
-
-
-  $(document).on("click", "#star a", function(event) {
-    event.preventDefault();
-    var star = $(this).find('.fa-star');
-    console.log(star);
-    if (star.hasClass("yellow-star")) {
-      star.removeClass("yellow-star");
-    }else {
-      star.addClass("yellow-star");
-    }
-});
-
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip({
-      container: 'body',
-      trigger: 'hover'
-  })
-})
-
-})
-
-
-
-// function tableFill(value){
-//   return `<tr>
-//         <td class="col-sm-1" id="tracknumber" ><div id="num">${value.song_order}</div></td>
-//         <td class="col-sm-1" id="star"><a href="" data-placement="top" title="MARK AS FAVORITE" data-toggle="tooltip" ><i class="fa fa-star" aria-hidden="true" style="color: #DDDDDD; font-size: 18px"></i></a></td>
-//         <td class="col-sm-3" id="songname">${value.song_name}</td>`
-//         if (${value.song_label} != null){
-//           `<td class="col-sm-1" id="category1"><div id="explicit">${value.song_label[0]}</div></td>`
-//         } else if (${value.song_label.length} == 2}) {
-//           `<td class="col-sm-7" id="category2"><div id="type">${value.song_label[1]}</div></td>`
-//         }
-//         `<td class="col-sm-1" id="songtime">${value.song_duration}</td>
-//       </tr>`;
-// }
-
-$(function(){
-  $.ajax({
-      url: "https://stg-resque.hakuapp.com/songs?album_id=1",
-      method: "GET",
-      dataType: 'jsonp'
-
-  })
-  .done(function( data ) {
-    $.each(data, function(index, value){
-      console.log(value);
-      // $('#paneltable').append(tableFill(value));
-    })
-  });
-
-})
-
-// $(function(){
-//   $.ajax({
-//       url: "https://stg-resque.hakuapp.com/songs?album_id=2",
-//       method: "GET",
-//       dataType: 'jsonp'
-//
-//   })
-//   .done(function( data ) {
-//     $.each(data, function(index, value){
-//       console.log(value);
-//       // $('#paneltable').append(tableFill(value));
-//     })
-//   });
-//
-// })
+}
